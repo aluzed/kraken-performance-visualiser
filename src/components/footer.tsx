c769d46532c12756fce93fad5048b61d1eb5d5d2
Aluzed <alexandre.penombre@gmail.com>
2024-02-26 20:12:38 +0100

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="fixed bottom-0 bg-white rounded-tl-lg rounded-tr-lg shadow-xl px-4 pt-2 flex flex-row justify-center">
         <button
           type="button"
-          className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray-300 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           onClick={() => setViewName("TRANSACTIONS_LIST")}
         >
           <FaThList />
@@ -18,7 +18,7 @@ export default function Footer() {
 
         <button
           type="button"
-          className="py-2.5 px-5 ms-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          className="py-2.5 px-5 ms-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray-300 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           onClick={() => setViewName("WALLET")}
         >
           <FaWallet />
